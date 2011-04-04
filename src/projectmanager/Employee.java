daf1d064834bce7d82a69e8f4e798c918afa13b7
Anders Emil Nielsen <andersemil91@gmail.com>
2011-04-04 14:39:17 +0200

    Lidt lulz

--- src/projectmanager/Employee.java
+++ src/projectmanager/Employee.java
@@ -3,7 +3,6 @@ package projectmanager;
 public class Employee {
 	private int userId;
 	private String name;
-	private String lulz;
 	public Employee(int userId, String name){
 		this.userId = userId;
 		this.name   = name;
