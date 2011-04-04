fc3265e1e291336a88dc63bf0eff224ae6de460d
Anders Emil Nielsen <andersemil91@gmail.com>
2011-04-04 14:17:54 +0200

    TWO NEW LINES!!

--- src/projectmanager/Employee.java
+++ src/projectmanager/Employee.java
@@ -3,6 +3,8 @@ package projectmanager;
 public class Employee {
 	private int userId;
 	private String name;
+	
+	
 	public Employee(int userId, String name){
 		this.userId = userId;
 		this.name   = name;
