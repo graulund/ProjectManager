03d8422b900829dee03ba37a3f8506ffb87384d2
Anders Emil Nielsen <andersemil91@gmail.com>
2011-04-04 14:58:02 +0200

    Udkommentering. ORHHH!!

--- test/projectmanager/LogInAsEmployee.java
+++ test/projectmanager/LogInAsEmployee.java
@@ -10,38 +10,34 @@ public class LogInAsEmployee {
 	/**
 	 * Tester scenariet, hvor en medarbejder logger ind
 	 */
-	@Test
-	public void testLogin() {
-		
-		ProjectManagerApp PMApp = new ProjectManagerApp();
-		
-		// checker at medarbejderen ikke er logget ind
-		assertFalse(PMApp.employeeLoggedIn());
-		
-		// medarbejder logger ind med initialer, der findes i databasen
-		boolean login = PMApp.employeeLogin("hlb");
-		
-		// checker at medarbejderen er logget ind
-		assertTrue(login);
-		assertTrue(PMApp.employeeLoggedIn());		
-	}
-	
-	@Test
-	public void testLoginFailed() {
-		ProjectManagerApp PMApp = new ProjectManagerApp();
-		
-		// checker at medarbejderen ikke er logget ind
-		assertFalse(PMApp.employeeLoggedIn());
-		
-		// medarbejder logger ind med initialer, der ikke findes i databasen (forkerte)
-		boolean login = PMApp.employeeLogin("lol");
-		
-		// checker at medarbejderen ikke er logget ind
-		assertFalse(login);
-		assertFalse(PMApp.employeeLoggedIn());	
-		
-		
-		
-	}
-
+//	@Test
+//	public void testLogin() {
+//		
+//		ProjectManagerApp PMApp = new ProjectManagerApp();
+//		
+//		// checker at medarbejderen ikke er logget ind
+//		assertFalse(PMApp.employeeLoggedIn());
+//		
+//		// medarbejder logger ind med initialer, der findes i databasen
+//		boolean login = PMApp.employeeLogin("hlb");
+//		
+//		// checker at medarbejderen er logget ind
+//		assertTrue(login);
+//		assertTrue(PMApp.employeeLoggedIn());		
+//	}
+//	
+//	@Test
+//	public void testLoginFailed() {
+//		ProjectManagerApp PMApp = new ProjectManagerApp();
+//		
+//		// checker at medarbejderen ikke er logget ind
+//		assertFalse(PMApp.employeeLoggedIn());
+//		
+//		// medarbejder logger ind med initialer, der ikke findes i databasen (forkerte)
+//		boolean login = PMApp.employeeLogin("lol");
+//		
+//		// checker at medarbejderen ikke er logget ind
+//		assertFalse(login);
+//		assertFalse(PMApp.employeeLoggedIn());	
+//	}
 }
