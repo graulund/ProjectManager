e7af31d0549a00281b4eb27dd64fbf6a0b984897
Andy Graulund <electricnet@gmail.com>
2011-04-04 14:06:45 +0200

    First classes

---
+++ src/projectmanager/Employee.java
@@ -0,0 +1,10 @@
+package projectmanager;
+
+public class Employee {
+	private int userId;
+	private String name;
+	public Employee(int userId, String name){
+		this.userId = userId;
+		this.name   = name;
+	}
+}
