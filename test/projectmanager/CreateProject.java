84f5bb3466b842cd464bb5ac1f322b12424c4485
Anders Emil Nielsen <andersemil91@gmail.com>
2011-04-04 15:26:49 +0200

    CreateProject

--- test/projectmanager/CreateProject.java
+++ test/projectmanager/CreateProject.java
@@ -5,21 +5,18 @@ import org.junit.Test;
 
 public class CreateProject {
 	
-	@Before
-	public void employeeLogsIn() {
-//		// checker at medarbejderen ikke er logget ind
-//		assertFalse(PMApp.employeeLoggedIn());
-//		
+//	@Before
+//	public void employeeLogsIn() {
 //		// medarbejder logger ind med initialer, der findes i databasen
 //		boolean login = PMApp.employeeLogin("hlb");
-	}
+//		assertTrue(PMApp.employeeLoggedIn());	
+//	}
 	
 	/**
 	 * Tester scenariet, hvor en medarbejder opretter et project.
 	 */
 	@Test
 	public void testCreateProject() {
-		
 	}
 
 }
