f78688bb777b9610b3b7f3632b4381b2c8ca9a6e
Anders Emil Nielsen <andersemil91@gmail.com>
2011-04-04 14:54:19 +0200

    Har lavet testen LoginAsEmployee. Is it lulz?

--- test/projectmanager/LogInAsEmployee.java
+++ test/projectmanager/LogInAsEmployee.java
@@ -1,6 +1,47 @@
 package projectmanager;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class LogInAsEmployee {
+	
+	/**
+	 * Tester scenariet, hvor en medarbejder logger ind
+	 */
+	@Test
+	public void testLogin() {
+		
+		ProjectManagerApp PMApp = new ProjectManagerApp();
+		
+		// checker at medarbejderen ikke er logget ind
+		assertFalse(PMApp.employeeLoggedIn());
+		
+		// medarbejder logger ind
+		boolean login = PMApp.employeeLogin("hlb");
+		
+		// checker at medarbejderen er logget ind
+		assertTrue(login);
+		assertTrue(PMApp.employeeLoggedIn());		
+	}
+	
+	@Test
+	public void testLoginFailed() {
+		ProjectManagerApp PMApp = new ProjectManagerApp();
+		
+		// checker at medarbejderen ikke er logget ind
+		assertFalse(PMApp.employeeLoggedIn());
+		
+		// medarbejder logger ind
+		boolean login = PMApp.employeeLogin("lol");
+		
+		// checker at medarbejderen er logget ind
+		assertFalse(login);
+		assertFalse(PMApp.employeeLoggedIn());	
+		
+		
+		
+	}
 
 }
