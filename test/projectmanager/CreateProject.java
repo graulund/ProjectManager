94e52607497b1310e94103806be4c50ea48db2ed
Anders Emil Nielsen <andersemil91@gmail.com>
2011-04-04 14:40:23 +0200

    TestUnit imported

---
+++ test/projectmanager/CreateProject.java
@@ -0,0 +1,15 @@
+package projectmanager;
+
+import org.junit.Test;
+
+public class CreateProject {
+	
+	/**
+	 * Tester scenariet, hvor en medarbejder opretter et project.
+	 */
+	@Test
+	public void testCreateProject() {
+		
+	}
+
+}
