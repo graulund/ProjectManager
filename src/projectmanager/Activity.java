156753835b654567117386a93ec6b1ee6242559b
Christian Zimmermann <klipklapper@gmail.com>
2011-04-10 19:08:52 +0200

    Activity added

---
+++ src/projectmanager/Activity.java
@@ -0,0 +1,61 @@
+package projectmanager;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class Activity {
+	private String name;
+	private Project project; // Cannot be changed after initiation
+	private List<Employee> employees = new ArrayList<Employee>();
+	private int start;  // week no. or calendar?
+	private int end;    // -||-
+	private int estimatedWorkHours;
+	
+	public Activity(String name, Project project){
+		this.setName(name);
+		this.project = project;
+	}
+	
+	public void addEmployee(Employee employee) {
+		this.employees.add(employee);
+	}
+	
+	public void removeEmployee(Employee employee) {
+		this.employees.remove(employee);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setStart(int week) {
+		this.start = week;
+	}
+	
+	public void setEnd(int week) {
+		this.end = week;
+	}
+	
+	public int getStart() {
+		return start;
+	}
+	
+	public int getEnd() {
+		return end;
+	}
+	
+	public void setWorkHours(int hours) {
+		estimatedWorkHours = hours;
+	}
+	
+	public int getWorkHours() {
+		return estimatedWorkHours;
+	}
+	
+}
--- test/projectmanager/CreateActivity.java
+++ test/projectmanager/CreateActivity.java
@@ -1,5 +1,21 @@
 package projectmanager;
 
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class CreateActivity {
 
+	/**
+	 * Main scenario: Opret aktivitet i et projekt
+	 */
+	@Test
+	public void CreateActivity() {
+		
+	}
 }
