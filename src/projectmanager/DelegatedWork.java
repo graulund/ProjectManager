335e0007f32be4719d693a9492eacab61d4c7839
Andy Graulund <electricnet@gmail.com>
2011-04-11 12:01:22 +0200

    Delegated work week

---
+++ src/projectmanager/DelegatedWork.java
@@ -0,0 +1,22 @@
+package projectmanager;
+
+public class DelegatedWork {
+	private int hours;
+	private Project project;
+	public DelegatedWork(int hours, Project project){
+		this.hours   = hours;
+		this.project = project;
+	}
+	public int getHours() {
+		return hours;
+	}
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	public Project getProject() {
+		return project;
+	}
+	public void setProject(Project project) {
+		this.project = project;
+	}
+}
---
+++ src/projectmanager/DelegatedWorkWeek.java
@@ -0,0 +1,68 @@
+package projectmanager;
+
+import java.util.ArrayList;
+
+public class DelegatedWorkWeek {
+	private Employee employee;
+	private int weekNumber;
+	private int year;
+	private ArrayList<DelegatedWork> delegatedWork   = new ArrayList<DelegatedWork>();
+	private ArrayList<RegisteredWork> registeredWork = new ArrayList<RegisteredWork>();
+	public DelegatedWorkWeek(Employee employee, int weekNumber, int year) {
+		this.employee   = employee;
+		this.weekNumber = weekNumber;
+		this.year       = year;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public int getWeekNumber() {
+		return weekNumber;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setTime(int weekNumber, int year) {
+		this.weekNumber = weekNumber;
+		this.year       = year;
+	}
+	public ArrayList<DelegatedWork> getDelegatedWork() {
+		return delegatedWork;
+	}
+	public void setDelegatedWork(ArrayList<DelegatedWork> delegatedWork) {
+		this.delegatedWork = delegatedWork;
+	}
+	public ArrayList<RegisteredWork> getRegisteredWork() {
+		return registeredWork;
+	}
+	public void setRegisteredWork(ArrayList<RegisteredWork> registeredWork) {
+		this.registeredWork = registeredWork;
+	}
+	public void addDelegatedWork(DelegatedWork work){
+		// TODO: What if it's already in the list?
+		this.delegatedWork.add(work);
+	}
+	public void addRegisteredWork(RegisteredWork work){
+		// TODO: What if it's already in the list?
+		this.registeredWork.add(work);
+	}
+	
+	public int getDelegatedHours(){
+		int total = 0;
+		for(DelegatedWork work: this.delegatedWork){
+			total += work.getHours();
+		}
+		return total;
+	}
+	
+//	public int getRegisteredHours(){
+//		int total = 0;
+//		for(RegisteredWork work: this.registeredWork){
+//			total += work.getHours();
+//		}
+//		return total;
+//	}
+}
