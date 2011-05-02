46f1199b1759d9a1625d63abb39e275cf84c4970
Andy Graulund <electricnet@gmail.com>
2011-05-02 11:33:08 +0200

    Character arrays, like a boss

--- src/projectmanager/app/StoredData.java
+++ src/projectmanager/app/StoredData.java
@@ -3,33 +3,33 @@ package projectmanager.app;
 public class StoredData {
 	// Storage structure:
 	private class StoredProject {
-		public String name;
-		public String client;
+		public char[] name;
+		public char[] client;
 		public int serialNumber;
-		public String projectLeaderName;
-		public String[] employeeNames;
+		public char[] projectLeaderName;
+		public char[][] employeeNames;
 		public StoredActivity[] activities;
 	}
 	
 	private class StoredEmployee {
-		public String name;
+		public char[] name;
 		public int[] workIds;
 	}
 	
 	private class StoredActivity {
-		public String name;
-		public String[] employeeNames;
+		public char[] name;
+		public char[][] employeeNames;
 		public int[] workIds;
-		public String startTime;
-		public String endTime;
+		public char[] startTime;
+		public char[] endTime;
 	}
 	
 	private class StoredWork {
 		public int workId;
 		public int weekNumber;
-		//public String[] employeeNames;
-		public String startTime;
-		public String endTime;
+		//public char[][] employeeNames;
+		public char[] startTime;
+		public char[] endTime;
 	}
 	
 	// Stored data for:
