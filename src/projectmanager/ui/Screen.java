891a94197c0eec5ff12f8145b442a92f0715babe
Andy Graulund <electricnet@gmail.com>
2011-05-02 20:16:09 +0200

    More UI + storage fixes

--- src/projectmanager/ui/Screen.java
+++ src/projectmanager/ui/Screen.java
@@ -29,4 +29,26 @@ abstract class Screen {
 	protected void wrongInputMessage(PrintWriter out){
 		out.println(this.wrong);
 	}
+	protected int parseNumberInput(String input, PrintWriter out){
+		int selection = -1;
+		try {
+			selection = Integer.parseInt(input);
+		} catch(NumberFormatException e){
+			this.wrongInputMessage(out);
+		}
+		return selection;
+	}
+	public String menuString(String[] choices, String cancel){
+		StringBuilder s = new StringBuilder();
+		for(int i = 0; i < choices.length; i++){
+			s.append((i+1) + ") " + choices[i] + "\n");
+		}
+		if(cancel != null && !cancel.isEmpty()){
+			s.append("0) " + cancel);
+		}
+		return s.toString();
+	}
+	public String menuString(String[] choices){
+		return menuString(choices, "Cancel");
+	}
 }
