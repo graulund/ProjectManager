c8914c550845cb7bdb7a81f6f60010a1ba08764d
Andy Graulund <electricnet@gmail.com>
2011-05-02 16:54:59 +0200

    Storage printing + test

--- src/projectmanager/app/AppStorage.java
+++ src/projectmanager/app/AppStorage.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
@@ -101,6 +102,62 @@ public class AppStorage {
 	public void loadState(){
 		
 	}
+	public void printState(StoredData data){
+		System.out.println(this.stateString(data));
+	}
+	public String stateString(StoredData data){
+		StringBuilder s = new StringBuilder("{ STORED DATA OBJECT\n");
+		s.append("-- Current serial number: " + data.currentSerialNumber + "\n");
+		s.append("-- Projects:\n");
+		for(StoredData.StoredProject project: data.projects){
+			s.append("   { PROJECT\n");
+			s.append("   -- Name: "); s.append(project.name);
+			s.append("\n   -- Client: "); s.append(project.client);
+			s.append("\n   -- Serial Number: "); s.append(project.serialNumber);
+			s.append("\n   -- Project Leader: "); s.append(project.projectLeaderName);
+			s.append("\n   -- Employees: "); s.append(Arrays.toString(this.parseStringArray(project.employeeNames)));
+			s.append("\n   -- Activities:\n");
+			for(StoredData.StoredActivity activity: project.activities){
+				s.append("      { ACTIVITY\n");
+				s.append("      -- Name: "); s.append(activity.name);
+				s.append("\n      -- Start time: "); s.append(activity.startTime);
+				s.append("\n      -- End time: "); s.append(activity.endTime);
+				s.append("\n      -- Employees: "); s.append(Arrays.toString(this.parseStringArray(activity.employeeNames)));
+				s.append("\n      -- Work: "); s.append(Arrays.toString(activity.workIds));
+				s.append("\n      }\n");
+			}
+			s.append("\n   }\n");
+		}
+		s.append("-- Employees: ");
+		for(StoredData.StoredEmployee employee: data.employees){
+			s.append("   { EMPLOYEE\n");
+			s.append("   -- Username: "); s.append(employee.username);
+			s.append("\n   -- Full name: "); s.append(employee.fullname);
+			s.append("\n   -- Work weeks:\n");
+			for(StoredData.StoredWorkWeek workweek: employee.workWeeks){
+				s.append("      { WORK WEEK\n");
+				s.append("      -- Week number: "); s.append(workweek.week);
+				s.append("\n      -- Year: "); s.append(workweek.year);
+				s.append("\n      -- Work: "); s.append(Arrays.toString(workweek.workIds));
+				s.append("\n      }\n");
+			}
+			s.append("\n   }\n");
+		}
+		s.append("-- Works: ");
+		for(StoredData.StoredWork work: data.works){
+			s.append("   { WORK\n");
+			s.append("   -- Work ID: "); s.append(work.workId);
+			if(work.halfHours > 0){
+				s.append("   -- Half hours: "); s.append(work.halfHours);
+			} else {
+				s.append("   -- Start time: "); s.append(work.startTime);
+				s.append("\n   -- End time: "); s.append(work.endTime);
+			}
+			s.append("\n   }\n");
+		}
+		s.append("\n}");
+		return s.toString();
+	}
 	public char[][] getEmployeeNames(List<Employee> employees){
 		ArrayList<char[]> names = new ArrayList<char[]>();
 		for(Employee e: employees){
@@ -149,4 +206,11 @@ public class AppStorage {
 //	public Calendar parseCalendarString(String s){
 //		
 //	}
+	public String[] parseStringArray(char[][] array){
+		String[] s = new String[array.length];
+		for(int i = 0; i < array.length; i++){
+			s[i] = new String(array[i]);
+		}
+		return s;
+	}
 }
