8546d4326c5eeda90ba853bab70ffc12f1af29ba
Anders Emil Nielsen <andersemil91@gmail.com>
2011-05-04 00:12:38 +0200

    Har rettet checkYourTime, så testen virker.

--- test/projectmanager/app/CheckYourTime.java
+++ test/projectmanager/app/CheckYourTime.java
@@ -40,16 +40,37 @@ public class CheckYourTime {
 		employee.addDelegatedWork(25, 2011, a1, 40);
 		employee.addDelegatedWork(25, 2011, a2, 30);
 		
-		employee.addRegisteredWork(new RegisteredWork(a1, 15));
-		employee.addRegisteredWork(new RegisteredWork(a2, 10));
+		Calendar startDate = new GregorianCalendar();
+		startDate.set(Calendar.YEAR, 2011);
+		startDate.set(Calendar.WEEK_OF_YEAR, 25);
+		startDate.set(Calendar.HOUR, 0);
+		startDate.set(Calendar.MINUTE, 0);
+		startDate.set(Calendar.SECOND, 0);
+		
+		Calendar endDate1 = new GregorianCalendar();
+		endDate1.set(Calendar.YEAR, 2011);
+		endDate1.set(Calendar.WEEK_OF_YEAR, 25);
+		endDate1.set(Calendar.HOUR, 15);
+		endDate1.set(Calendar.MINUTE, 0);
+		endDate1.set(Calendar.SECOND, 0);
+		
+		Calendar startDate2 = (Calendar) startDate.clone();
+		Calendar endDate2 = (Calendar) endDate1.clone();
+		startDate2.add(Calendar.DATE, 1);
+		endDate2.add(Calendar.DATE, 1);
+		endDate2.add(Calendar.HOUR, -5);
+		
+		RegisteredWork regwork1 = new RegisteredWork(a1, startDate, endDate1); // 15 hours of work
+		RegisteredWork regwork2 = new RegisteredWork(a2, startDate2, endDate2); // 10 hours of work
+		employee.addRegisteredWork(regwork1);
+		employee.addRegisteredWork(regwork2);
 		
 		// Vi forventer nu at få følgende resultat
-		String one = "funky activity: 0/40"; // should be 15/40
-		String two = "sweet activity: 0/30"; // should be 10/30
+		String one = "funky activity: 15/40"; 
+		String two = "sweet activity: 10/30"; 	
 		
 		// Tjekker schedule
-		List<String> schedule = employee.getWorkWeeks().get(0).getWeekSchedule();
-		
+		List<String> schedule = employee.getWorkWeek(25, 2011).getWeekSchedule();
 		assertEquals(schedule.get(0), one);
 		assertEquals(schedule.get(1), two);
 	}
