92b398277bda47ab335e64eb1a90c2e7e314580f
Anders Emil Nielsen <andersemil91@gmail.com>
2011-05-05 15:38:23 +0200

    nothing

--- src/projectmanager/ui/SeeYourTimeScreen.java
+++ src/projectmanager/ui/SeeYourTimeScreen.java
@@ -1,8 +1,15 @@
 package projectmanager.ui;
 import java.io.PrintWriter;
+import java.util.List;
+
+import projectmanager.app.Employee;
+import projectmanager.app.Project;
+import projectmanager.app.ProjectManagerApp;
 
 
 public class SeeYourTimeScreen extends Screen {
+	private String[] choices;
+	private int[] numbers;
 
 	@Override
 	void printMenu(PrintWriter out) {
@@ -10,14 +17,13 @@ public class SeeYourTimeScreen extends Screen {
 				this.formatTitle("See your time") +
 				this.menuString(new String[]{ "" }, "Back")
 			);
-
 	}
 
 	@Override
 	boolean processInput(String input, PrintWriter out) {
 		int selection = this.parseNumberInput(input, out);
 		this.clearScreen(out);
-		switch(selection){
+		switch(selection) {
 			case 0:
 				this.ui.setScreen(new TimeMenuScreen());
 				break;
