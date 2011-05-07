a79016c6d999197f8d9cd6df1630643c15fd9bfb
Andy Graulund <electricnet@gmail.com>
2011-05-07 22:17:30 +0200

    TestSaveObject now prints current SampleDataSetup

--- test/projectmanager/app/TestSaveObject.java
+++ test/projectmanager/app/TestSaveObject.java
@@ -1,26 +1,27 @@
 package projectmanager.app;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-public class TestSaveObject {//extends SampleDataSetup {
+public class TestSaveObject extends SampleDataSetup {
 	@Test
 	public void printSaveObject(){
 		AppStorage storage = new AppStorage();
+		
 		//StoredData obj = new StoredData();
 		//StoredData obj     = storage.storeCurrentState();
 		//storage.printState(obj);
 		//storage.saveState(obj);
 		//storage.saveCurrentState();
-		StoredData obj = storage.loadState();
+		
+		StoredData obj = storage.storeCurrentState();
 		storage.printState(obj);
-		storage.restoreState(obj);
-		ProjectManagerApp.employeeLogin("ridp");
+		
+		//storage.restoreState(obj);
+		/*ProjectManagerApp.employeeLogin("ridp");
 		assertTrue(ProjectManagerApp.isEmployeeLoggedIn());
 		Employee employee = ProjectManagerApp.getEmployeeLoggedIn();
 		System.out.println(employee);
 		System.out.println(employee.getUsername());
-		System.out.println(employee.getWorkWeeks().size());
+		System.out.println(employee.getWorkWeeks().size());*/
 	}
 }
