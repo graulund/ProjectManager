690cac95889566190f21d6bf0285a28952c2c993
Anders Emil Nielsen <andersemil91@gmail.com>
2011-05-08 22:18:56 +0200

    test

--- test/projectmanager/app/ChooseYourselfAsProjectLeader.java
+++ test/projectmanager/app/ChooseYourselfAsProjectLeader.java
@@ -57,44 +57,5 @@ public class ChooseYourselfAsProjectLeader {
 		
 		// checker at medarbejderen er angivet som projektleder
 		assertEquals(ProjectManagerApp.getEmployeeLoggedIn(), projectTest.getLeader());
-	}
-	
-	/**
-	 * Alternative scenario: Tester scenariet, hvor en medarbejder pr¿ver at
-	 * tilf¿je sig selv som projektleder, men bliver afvist, idet
-	 * der allerede er tildelt en projektleder.
-	 */
-	@Test
-	public void ProjectLeaderAlreadyExists() {
-		Employee employee1 = new Employee("hlb");
-		Employee employee2 = new Employee("abc");
-		Company company = ProjectManagerApp.getCompany();
-		company.addEmployee(employee1);
-		company.addEmployee(employee2);
-		
-		// medarbejder logger ind med initialer, der findes i databasen
-		boolean login = ProjectManagerApp.employeeLogin("hlb");
-		
-		// checker at medarbejderen er logget ind
-		assertTrue(login);
-		assertTrue(ProjectManagerApp.isEmployeeLoggedIn());
-		
-		
-		// projekt med l¿benummer 001 oprettes
-		String name = "Software Engineering";
-		String client = "Google";
-		Project project1 = new Project(name, client);
-		
-		// tilf¿jer projekt til firmaet
-		company.addProject(project1);
-		project1.addLeader(employee2);
-		
-		// medarbejder tilf¿jer sig selv som projektleder
-		project1.addLeader(ProjectManagerApp.getEmployeeLoggedIn());
-		
-		// da der allerede er tilf¿jet et projekt, skal medarbejderen,
-		// der er logget ind, ikke v¾re projektleder.
-		assertEquals(employee2, project1.getLeader());
-	}
-		
+	}		
 }
