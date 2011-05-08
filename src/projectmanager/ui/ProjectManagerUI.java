34d40423835cc0e4b046419297293e3fd35040f2
Andy Graulund <electricnet@gmail.com>
2011-05-08 14:00:48 +0200

    UI flow

--- src/projectmanager/ui/ProjectManagerUI.java
+++ src/projectmanager/ui/ProjectManagerUI.java
@@ -11,19 +11,24 @@ import projectmanager.app.ProjectManagerApp;
  */
 public class ProjectManagerUI {
 	/**
-	 * Link to the main App object
+	 * Link to the main App object.
 	 */
 	private ProjectManagerApp app = new ProjectManagerApp();
 	
 	/**
-	 * Link to the current Screen object
+	 * Link to the current Screen object.
 	 */
 	private Screen screen;
 	
 	/**
-	 * Link to the current BufferedReader object from which to get input from the user
+	 * Link to the current BufferedReader object from which to get input from the user.
 	 */
 	public BufferedReader in;
+	
+	/**
+	 * Boolean value determining whether we're in a flow that needs to change screen automatically.
+	 */
+	private boolean flow = false;
 	
 	/**
 	 * Creates a new UI with the log in screen displayed.
@@ -34,15 +39,15 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Creates a new UI.
-	 * @param screen Screen object to be displayed
+	 * @param screen Screen object to be displayed.
 	 */
 	public ProjectManagerUI(Screen screen){
 		this.setScreen(screen);
 	}
 	
 	/**
-	 * Prints the menu/information in the current screen
-	 * @param out PrintWriter object being written to
+	 * Prints the menu/information in the current screen.
+	 * @param out PrintWriter object being written to.
 	 * @throws IOException
 	 */
 	public void printMenu(PrintWriter out) throws IOException {
@@ -51,7 +56,7 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Reads user input into the current screen.
-	 * @param in BufferedReader object being read from
+	 * @param in BufferedReader object being read from.
 	 * @return String
 	 * @throws IOException
 	 */
@@ -61,9 +66,9 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Processes the input from the user in the screen.
-	 * @param input Input from the user given as a String
-	 * @param out PrintWriter object that can be written to
-	 * @return boolean describing whether it's okay to quit the app
+	 * @param input Input from the user given as a String.
+	 * @param out PrintWriter object that can be written to.
+	 * @return boolean describing whether it's okay to quit the app.
 	 * @throws IOException
 	 */
 	public boolean processInput(String input, PrintWriter out) throws IOException {
@@ -72,31 +77,37 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Runs the basic UI loop that prints the current menu and waits for input.
-	 * @param in BufferedReader object being read from
-	 * @param out PrintWriter object being written to
+	 * @param in BufferedReader object being read from.
+	 * @param out PrintWriter object being written to.
 	 * @throws IOException
 	 */
 	public void basicLoop(BufferedReader in, PrintWriter out) throws IOException {
 		String selection;
 		do {
 			this.printMenu(out);
-			System.out.print("> ");
-			selection = this.readInput(in);
+			if(!this.flow){
+				System.out.print("> ");
+				selection = this.readInput(in);
+			} else {
+				this.flow = false;
+				selection = "0";
+			}
 		} while (!this.processInput(selection, out));
 	}
 	
 	/**
 	 * Sets the current screen.
-	 * @param screen Screen object
+	 * @param screen Screen object.
 	 */
 	void setScreen(Screen screen) {
 		this.screen    = screen;
 		this.screen.ui = this;
+		this.flow      = true;
 	}
 	
 	/**
 	 * Gets the current screen.
-	 * @return Screen object
+	 * @return Screen object.
 	 */
 	Screen getScreen() {
 		return this.screen;
@@ -104,7 +115,7 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Gets the current input object.
-	 * @return BufferedReader object being read from
+	 * @return BufferedReader object being read from.
 	 */
 	public BufferedReader getIn() {
 		return in;
@@ -112,7 +123,7 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Sets the current input object.
-	 * @param in BufferedReader object being read from
+	 * @param in BufferedReader object being read from.
 	 */
 	public void setIn(BufferedReader in) {
 		this.in = in;
@@ -120,7 +131,7 @@ public class ProjectManagerUI {
 	
 	/**
 	 * Gets the App object.
-	 * @return ProjectManagerApp object
+	 * @return ProjectManagerApp object.
 	 */
 	public ProjectManagerApp getApp() {
 		return app;
