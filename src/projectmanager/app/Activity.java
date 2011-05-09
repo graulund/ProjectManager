7a3e92eb94321eddda5732510c104a5b796bdb4c
Christian Zimmermann <klipklapper@gmail.com>
2011-05-09 12:20:59 +0200

    Javadoc on Project+Activity

--- src/projectmanager/app/Activity.java
+++ src/projectmanager/app/Activity.java
@@ -4,43 +4,89 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
-
+/**
+ * A class that defines an activity.
+ * An activity is a part of a project. It contains a list of employees assigned to it, and lists of the work they do.
+ */
 public class Activity {
+	/**
+	 * Name of activity.
+	 */
 	private String name;
+	/**
+	 * List of employees assigned to the activity.
+	 */
 	private List<Employee> employees = new ArrayList<Employee>();
+	/**
+	 * List of delegated work.
+	 */
 	private List<DelegatedWork> del_works = new ArrayList<DelegatedWork>();
+	/**
+	 * List of registered work.
+	 */
 	private List<RegisteredWork> reg_works = new ArrayList<RegisteredWork>();
+	/**
+	 * Start of activity.
+	 */
 	private Calendar start;
+	/**
+	 * End of activity.
+	 */
 	private Calendar end;
 	
+	/**
+	 * Constructor.
+	 * @param name
+	 */
 	public Activity(String name){
 		this.setName(name);
 	}
-	
+	/**
+	 * Adds employee to activity.
+	 * @param employee
+	 */
 	public void addEmployee(Employee employee) {
 		this.employees.add(employee);
 	}
-	
+	/**
+	 * Removes employee from activity.
+	 * @param employee
+	 */
 	public void removeEmployee(Employee employee) {
 		this.employees.remove(employee);
 	}
-	
+	/**
+	 * Returns a list of employees assigned to activity.
+	 * @return
+	 */
 	public List<Employee> getEmployees() {
 		return employees;
 	}
-
+	/**
+	 * Overwrites the list of employees.
+	 * @param employees
+	 */
 	public void setEmployees(List<Employee> employees) {
 		this.employees = employees;
 	}
-
+	/**
+	 * Adds registered work to activity.
+	 * @param rw
+	 */
 	public void addRegisteredWork(RegisteredWork rw) {
 		this.reg_works.add(rw);
 	}
-	
+	/**
+	 * Adds delegated work to activity.
+	 * @param dw
+	 */
 	public void addDelegatedWork(DelegatedWork dw) {
 		this.del_works.add(dw);
 	}
-	
+	/**
+	 * Adds any kind of work to activity.
+	 * @param w
+	 */
 	public void addWork(Work w){
 		if(w != null){
 			if(w.getClass() == RegisteredWork.class){
@@ -51,15 +97,26 @@ public class Activity {
 			}
 		}
 	}
-	
+	/**
+	 * Sets the name of activity.
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	/**
+	 * Gets name of activity.
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
-	
+	/**
+	 * Sets start date on activity.
+	 * @param week
+	 * @param year
+	 * @throws ProjectManagerException
+	 */
 	public void setStart(int week, int year) throws ProjectManagerException {
 		if (this.end != null) {
 			if (this.end.get(Calendar.YEAR) == year && this.end.get(Calendar.WEEK_OF_YEAR) < week) {
@@ -72,7 +129,12 @@ public class Activity {
 		this.start.set(Calendar.YEAR, year);
 		this.start.set(Calendar.WEEK_OF_YEAR, week);
 	}
-	
+	/**
+	 * Sets end date on activity.
+	 * @param week
+	 * @param year
+	 * @throws ProjectManagerException
+	 */
 	public void setEnd(int week, int year) throws ProjectManagerException {
 		if (this.start != null) {
 			if (this.start.get(Calendar.YEAR) == year && this.start.get(Calendar.WEEK_OF_YEAR) > week) {
@@ -86,23 +148,38 @@ public class Activity {
 		this.end.set(Calendar.WEEK_OF_YEAR, week);	
 			
 	}
-
+	/**
+	 * Sets start date on activity.
+	 * @param start
+	 */
 	public void setStart(Calendar start) {
 		this.start = start;
 	}
-
+	/**
+	 * Sets end date on activity.
+	 * @param end
+	 */
 	public void setEnd(Calendar end) {
 		this.end = end;
 	}
-
+	/**
+	 * Returns start date.
+	 * @return
+	 */
 	public Calendar getStart() {
 		return this.start;
 	}
-	
+	/**
+	 * Returns end date.
+	 * @return
+	 */
 	public Calendar getEnd() {
 		return this.end;
 	}
-	
+	/**
+	 * Returns number of delegated hours assigned to activity.
+	 * @return
+	 */
 	public int getDelegatedHours() {
 		int total = 0;
 		for (DelegatedWork del_work: this.del_works) {
@@ -110,7 +187,10 @@ public class Activity {
 		}
 		return total;
 	}
-	
+	/**
+	 * Returns number of registered hours on activity.
+	 * @return
+	 */
 	public double getRegisteredHours() {
 		double total = 0;
 		for (RegisteredWork reg_work: this.reg_works) {
@@ -118,23 +198,38 @@ public class Activity {
 		}
 		return total;
 	}
-
+	/**
+	 * Returns the list of delegated work.
+	 * @return
+	 */
 	public List<DelegatedWork> getDelegatedWork() {
 		return del_works;
 	}
-
+	/**
+	 * Sets the list of delegated work.
+	 * @param del_works
+	 */
 	public void setDelegatedWork(List<DelegatedWork> del_works) {
 		this.del_works = del_works;
 	}
-
+	/**
+	 * Returns the list of registered work.
+	 * @return
+	 */
 	public List<RegisteredWork> getRegisteredWork() {
 		return reg_works;
 	}
-
+	/**
+	 * Sets the list of registered work.
+	 * @param reg_works
+	 */
 	public void setRegisteredWork(List<RegisteredWork> reg_works) {
 		this.reg_works = reg_works;
 	}
-	
+	/**
+	 * Gets a list of all Work objects.
+	 * @return
+	 */
 	public List<Work> getWork(){
 		List<Work> works = new ArrayList<Work>();
 		for(DelegatedWork w: this.del_works){
--- src/projectmanager/app/Project.java
+++ src/projectmanager/app/Project.java
@@ -2,42 +2,101 @@ package projectmanager.app;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * A class that defines a project.
+ * A project contains a number of activities and employees, but only a single project leader.
+ */
 public class Project {
+	/**
+	 * Name of project.
+	 */
 	private String name;
+	/**
+	 * Name of client.
+	 */
 	private String client;
+	/**
+	 * Serial number used mainly by AppStorage class.
+	 */
 	private int serialNumber;
+	/**
+	 * The Employee object that acts as a leader on the project.
+	 */
 	private Employee projectLeader;
+	/**
+	 * List of Employees assigned to the project.
+	 */
 	private List<Employee> employees = new ArrayList<Employee>();
+	/**
+	 * List of Activities in the project.
+	 */
 	private List<Activity> activities = new ArrayList<Activity>();
 
-	
+	/**
+	 * Constructor.
+	 * @param name
+	 * @param client
+	 */
 	public Project(String name, String client) {
 		this(name, client, ProjectManagerApp.newSerialNumber());
 	}
+	/**
+	 * Constructor used by AppStorage when loading a state.
+	 * @param name
+	 * @param client
+	 * @param serialNumber
+	 */
 	public Project(String name, String client, int serialNumber){
 		this.serialNumber = serialNumber;
 		this.name = name;
 		this.client = client;
 	}
+	/**
+	 * Gets name of project.
+	 * @return
+	 */
 	public String getName(){
 		return this.name;
 	}
+	/**
+	 * Gets name of client.
+	 * @return
+	 */
 	public String getClient(){
 		return this.client;
 	}
+	/**
+	 * Sets name of project.
+	 * @param name
+	 */
 	public void setName(String name){
 		this.name = name;
 	}
+	/**
+	 * Sets name of client.
+	 * @param client
+	 */
 	public void setClient(String client){
 		this.client = client;
 	}
+	/**
+	 * Sets serial number.
+	 * @param serialKey
+	 */
 	public void setSerialNumber(int serialKey) {
 		this.serialNumber = serialKey;
 	}
+	/**
+	 * Gets serial number.
+	 * @return
+	 */
 	public int getSerialNumber() {
 		return this.serialNumber;
 	}
+	/**
+	 * Sets employee as leader on project.
+	 * @param employee
+	 */
 	public void addLeader(Employee employee) {
 		if (this.projectLeader != null) {
 			this.projectLeader.removeProjectLeaderOf(this);
@@ -45,9 +104,19 @@ public class Project {
 		this.projectLeader = employee;
 		employee.setProjectLeaderOf(this);
 	}
+	/**
+	 * Gets leader of project.
+	 * @return
+	 */
 	public Employee getLeader() {
 		return this.projectLeader;
 	}
+	/**
+	 * Adds activity to project.
+	 * @param activity
+	 * @throws OperationNotAllowedException
+	 * @throws CreatingActivityException
+	 */
 	public void addActivity(Activity activity) throws OperationNotAllowedException, CreatingActivityException {
 		if (this.projectLeader != null) {
 			if (ProjectManagerApp.getEmployeeLoggedIn() != this.projectLeader)
@@ -57,13 +126,24 @@ public class Project {
 			this.activities.add(activity);
 		}	
 	}
+	/**
+	 * Removes activity from project.
+	 * @param activity
+	 */
 	public void removeActivity(Activity activity) {
 		this.activities.remove(activity);
 	}
+	/**
+	 * Gets list of activities.
+	 * @return
+	 */
 	public List<Activity> getActivities() {
 		return activities;
 	}
-
+	/**
+	 * Returns a report on the project.
+	 * @return
+	 */
 	public String getReport() {
 		double registeredHours = 0;
 		int delegatedHours = 0;
@@ -73,6 +153,11 @@ public class Project {
 		}
 		return "Project: "+name+". Client: "+client+". Project leader: "+projectLeader.getUsername()+". Work status: "+Math.round(registeredHours/2)+"/"+delegatedHours/2+".";
 	}
+	/**
+	 * Checks if project has an activity with a certain name and returns it.
+	 * @param name
+	 * @return
+	 */
 	public Activity activityByName(String name) {
 		for (Activity activity: this.activities) {
 			if (activity.getName().equals(name)) {
@@ -81,23 +166,48 @@ public class Project {
 		}
 		return null;
 	}
-	
+	/**
+	 * Adds employee to project and activity.
+	 * @param employee
+	 * @param activity
+	 */
 	public void addEmployee(Employee employee, Activity activity) {
 		this.employees.add(employee);
 		activity.addEmployee(employee);
 	}
+	/**
+	 * Adds employee to project.
+	 * Used mainly by AppStorage.
+	 * @param employee
+	 */
 	public void addEmployee(Employee employee) {
 		this.employees.add(employee);
 	}
+	/**
+	 * Removes employee from project.
+	 * @param employee
+	 */
 	public void removeEmployee(Employee employee) {
 		this.employees.remove(employee);
 	}
+	/**
+	 * Returns a list of employees on project.
+	 * @return
+	 */
 	public List<Employee> getEmployees() {
 		return employees;
 	}
+	/**
+	 * Overwrites the projects list of employees with the given one.
+	 * @param employees
+	 */
 	public void setEmployees(List<Employee> employees) {
 		this.employees = employees;
 	}
+	/**
+	 * Checks if project has a leader.
+	 * @return
+	 */
 	public boolean hasProjectManager() {
 		if (projectLeader == null) return false;
 		else return true;
