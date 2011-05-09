dbd65d41f99c70cf4f7e8e15abbccf57d70dfb0b
Andy Graulund <electricnet@gmail.com>
2011-05-09 14:24:10 +0200

    Updated Javadoc and Readme

--- README_22.txt
+++ README_22.txt
@@ -1,11 +1,19 @@
-Overview:
+== PROJECT MANAGER ===================================================
+-- Gruppe 22
+
+-- Overblik ----------------------------------------------------------
 Applikationen: src > projectmanager > app > ProjectManagerApp.java
 Cmd line interface: src > projectmanager > ui > ProjectManagerUI.java
 Systematiske tests: test > projectmanager > app
 
-Instructions:
-Til at køre CLI'et får I brug for employee usernames. Vi har uploadet et "state" med, hvor der allerede er registreret employees og projekter. Dertil kan I bruge usernames: aid, qvqi, sjhd.
+-- Instruktioner -----------------------------------------------------
+Til at køre CLI'et får I brug for employee usernames. Vi har uploadet 
+et "state" med (i pmapp.data), hvor der allerede er registreret 
+employees og projekter. Dertil kan I bruge flg. brugernavne: 
+	aid, qvqi, sjhd
 
-Hvis I vil lave et nyt state kan I køre TestSaveObject.java (under tests). Men husk at tjekke det den printer ud efter usernames, så I kan logge ind.
+Hvis I vil lave et nyt state kan I køre TestSaveObject.java (under 
+tests). Men husk at tjekke de brugernavne, den printer ud blandt alle 
+data, så I kan logge ind.
 
 God fornøjelse!
--- src/projectmanager/app/Company.java
+++ src/projectmanager/app/Company.java
@@ -3,26 +3,53 @@ package projectmanager.app;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Company object keeps hold of projects and employees
+ * in this application.
+ */
 public class Company {
+	/**
+	 * List of projects which has been created by this company.
+	 */
 	private List<Project> projects = new ArrayList<Project>();
+	
+	/**
+	 * List of employees hired.
+	 */
 	private List<Employee> employees = new ArrayList<Employee>();
 	
 	/**
-	 * Reference to the current Company object
+	 * Reference to the current Company object.
 	 */
 	public static Company c;
 	
+	/**
+	 * Company constructor.
+	 */
 	public Company(){
 		c = this;
 	}
 	
+	/**
+	 * Returns the list of all projects by this company.
+	 * @return list of Project objects.
+	 */
 	public List<Project> getProjects() {
 		return this.projects;
 	}
+	
+	/**
+	 * Sets the list of projects.
+	 * @param list of Project objects.
+	 */
 	public void setProjects(List<Project> projects) {
 		this.projects = projects;
 	}
-
+	
+	/**
+	 * Adds a project to the company
+	 * @param project
+	 */
 	public void addProject(Project project) {
 		for(Project p: this.projects){
 			if(p == project){ // This project already present
@@ -32,6 +59,10 @@ public class Company {
 		this.projects.add(project);
 	}
 	
+	/**
+	 * Adds an employee to the company
+	 * @param employee
+	 */
 	public void addEmployee(Employee employee) {
 		for(Employee e: this.employees){
 			if(e == employee){ // This employee already present
@@ -41,15 +72,28 @@ public class Company {
 		this.employees.add(employee);
 	}
 	
+	/**
+	 * Returns the list of employees
+	 * @return list of employees
+	 */
 	public List<Employee> getEmployees() {
 		return employees;
 	}
 	
+	/**
+	 * Sets the list of employees
+	 * @param employees
+	 */
 	public void setEmployees(ArrayList<Employee> employees){
 		this.employees = employees;
 		//this.currentSerialNumber = this.employees.size() + 1;
 	}
 	
+	/**
+	 * Returns an employee by the given username
+	 * @param username
+	 * @return employee
+	 */
 	public Employee employeeByUsername(String username) {
 		for (Employee employee: this.employees) {
 			if (username.equals(employee.getUsername())) {
@@ -59,6 +103,11 @@ public class Company {
 		return null;
 	}
 	
+	/**
+	 * Returns a project by the given serialnumber
+	 * @param serialNumber
+	 * @return project
+	 */
 	public Project projectBySerialNumber(int serialNumber) {
 		for (Project project: this.projects) {
 			if (serialNumber == project.getSerialNumber()) {
@@ -67,13 +116,11 @@ public class Company {
 		}
 		return null;
 	}
-	
-	public String getEmployeeAvailability() {
-		return "On coffee break."; // TODO: FIX THIS!
-		
-		// Should return a list of all employees and their (near) future availability
-	}
 
+	/**
+	 * Returns a report for this company
+	 * @return report (String)
+	 */
 	public String getReport() {
 		String report = "";
 		for (Project p: projects) {
